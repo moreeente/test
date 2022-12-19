f395a72956d3ce6087fe4a21c98851b0aeb4290b
moreeente <121024551+moreeente@users.noreply.github.com>
2022-12-19 12:14:10 +0100

    morente

--- contador.php
+++ contador.php
@@ -1,1 +1,11 @@
-echo "hola que tal"
+<?php 
+
+    $count=$_COOKIE ['count']; 
+    if(isset($count)) { 
+                $count++; 
+    }else  {
+                $count=0; 
+    }
+    setcookie('count', $count); 
+    echo $count;
+?>
